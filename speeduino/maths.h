1141dc78258ba0a4d00cb96bd8493e06c1b0baa5
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2021-12-22 19:38:37 -0600

    table3d interpolation performance 1. Use narrowest type possible 2. Use unsigned types

--- speeduino/maths.h
+++ speeduino/maths.h
@@ -2,7 +2,6 @@
 #define MATH_H
 
 #include <stdint.h>
-#include "globals.h"
 
 #ifdef USE_LIBDIVIDE
 // We use pre-computed constant parameters with libdivide where possible. 
@@ -346,5 +345,82 @@ static inline int16_t LOW_PASS_FILTER(int16_t input, uint8_t alpha, int16_t prio
   int32_t preshift = ((input * (int32_t)inv_alpha) + prior_alpha);
   return (uint32_t)preshift >> 8U;
 }
+
+// We want to use the gcc intrinsic function here.
+#pragma push_macro("abs")
+#undef abs
+
+/**
+ * @defgroup muldiv muldiv
+ * @brief Optimised calculation of (a*b)/c.
+ *
+ * This family of functions will calculate (a*b)/c using the narrowest type possible,
+ * which should be the fastest method on AVR. Division is implemented in
+ * software so narrower types == faster division.
+ * 
+ * (a*b)/c is often used to scale a value from one range to another.
+ * 
+ * Notes:
+ * 1. The return type is the same as the inputs, so <b>THESE SHOULD NOT BE CALLED IF THE RESULT WOULD OVERFLOW THE RETURN TYPE</b>
+ * 2. The optimization only works on 16-bit numbers and only for small values of a and b (less than sqrt([u]int16_t])).
+ *    Additional overloads for other types are provided for uniformity.
+ * 
+ * @param a Multiplier 1    
+ * @param b Multiplier 2
+ * @param c The divisor
+ * @return The result of (a*b)/c
+ * @{
+ * */
+inline int8_t muldiv(int8_t a, int8_t b, int8_t div)
+{
+    // By the time we condtionally check for potential overflow, including
+    // accounting for negative operands, we are just as quick promoting
+    // to int16_t & applying the multiply + divide.
+    return ((int16_t)a * (int16_t)b) / (int16_t)div;
+}
+
+inline uint8_t muldiv(uint8_t a, uint8_t b, uint8_t div)
+{
+    // By the time we condtionally check for potential overflow,
+    // we are just as quick promoting to int16_t & applying the 
+    // multiply + divide.
+    return ((uint16_t)a * (uint16_t)b) / (uint16_t)div;
+}
+
+inline int16_t muldiv(const int16_t &a, const int16_t &b, const int16_t &div)
+{
+    constexpr int16_t overflow_threshold = 182; // sqrt(INT16_MAX) + 1
+    if (abs(a)<overflow_threshold && abs(b)<overflow_threshold)
+    {
+        int16_t numerator = a * b;
+        return numerator / div;
+    }
+    return ((int32_t)a * (int32_t)b) / (int32_t)div;
+}
+
+inline uint16_t muldiv(const uint16_t &a, const uint16_t &b, const uint16_t &div)
+{
+    return udiv_32_16((uint32_t)a * (uint32_t)b, div);
+}
+
+inline int32_t muldiv(const int32_t &a, const int32_t &b, const int32_t &div)
+{
+    return (a * b) / div;
+}
+
+inline uint32_t muldiv(const uint32_t &a, const uint32_t &b, const uint32_t &div)
+{
+    uint32_t dividend = a * b;
+#ifdef USE_LIBDIVIDE    
+    if (div<=(uint32_t)UINT16_MAX && udiv_is16bit_result(dividend, div)) {
+        return udiv_32_16(dividend, (uint16_t)div);
+    }
+#endif
+    return dividend / div;
+}
+
+#pragma pop_macro("abs")
+
+/** @} */
 
 #endif
