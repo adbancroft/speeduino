9d04e4055bfda62b281b2b9302881aab54bc41bb
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2023-07-01 22:12:57 -0500

    Use an array instead of hard coded variables.

--- test/test_decoders/FordST170/ForsdST170.cpp
+++ test/test_decoders/FordST170/ForsdST170.cpp
@@ -4,11 +4,15 @@
 #include "FordST170.h"
 #include "scheduler.h"
 #include "../../test_utils.h"
+#include "utilities.h"
 
-extern uint16_t ignition1EndTooth;
-extern uint16_t ignition2EndTooth;
-extern uint16_t ignition3EndTooth;
-extern uint16_t ignition4EndTooth;
+extern uint16_t ignitionEndTeeth[_countof(ignitionSchedules)];
+
+void testSetup_FordST170(void)
+{
+    triggerSetup_FordST170();
+    maxIgnOutputs = 4;
+}
 
 void test_fordst170_newIgn_12_trig0_1()
 {
@@ -16,25 +20,25 @@ void test_fordst170_newIgn_12_trig0_1()
     //Trigger: 12/1
     //Advance: 10
     //triggerAngle=0
-    triggerSetup_FordST170();
+    testSetup_FordST170();
     configPage4.sparkMode = IGN_MODE_WASTED;
     configPage4.triggerAngle = 0; //No trigger offset
     calculateIgnitionAngles(ignitionSchedules[0], 5, 10);
   
     triggerSetEndTeeth_FordST170();
-    TEST_ASSERT_EQUAL(34, ignition1EndTooth);
+    TEST_ASSERT_EQUAL(34, ignitionEndTeeth[0]);
 
     //Test again with 0 degrees advance
     calculateIgnitionAngles(ignitionSchedules[0], 5, 0);
 
     triggerSetEndTeeth_FordST170();
-    TEST_ASSERT_EQUAL(35, ignition1EndTooth);
+    TEST_ASSERT_EQUAL(35, ignitionEndTeeth[0]);
 
     //Test again with 35 degrees advance
     calculateIgnitionAngles(ignitionSchedules[0], 5, 35);
 
     triggerSetEndTeeth_FordST170();
-    TEST_ASSERT_EQUAL(31, ignition1EndTooth);
+    TEST_ASSERT_EQUAL(31, ignitionEndTeeth[0]);
 }
 
 void test_fordst170_newIgn_12_trig90_1()
@@ -43,13 +47,13 @@ void test_fordst170_newIgn_12_trig90_1()
     //Trigger: 12/1
     //Advance: 10
     //triggerAngle=90
-    triggerSetup_FordST170();
+    testSetup_FordST170();
     configPage4.sparkMode = IGN_MODE_WASTED;
     configPage4.triggerAngle = 90; //No trigger offset
     calculateIgnitionAngles(ignitionSchedules[0], 5, 35);
 
     triggerSetEndTeeth_FordST170();
-    TEST_ASSERT_EQUAL(22, ignition1EndTooth);
+    TEST_ASSERT_EQUAL(22, ignitionEndTeeth[0]);
 }
 
 void test_fordst170_newIgn_12_trig180_1()
@@ -58,13 +62,13 @@ void test_fordst170_newIgn_12_trig180_1()
     //Trigger: 36-1
     //Advance: 10
     //triggerAngle=180
-    triggerSetup_FordST170();
+    testSetup_FordST170();
     configPage4.sparkMode = IGN_MODE_WASTED;
     configPage4.triggerAngle = 180; //No trigger offset
     calculateIgnitionAngles(ignitionSchedules[0], 5, 10);
  
     triggerSetEndTeeth_FordST170();
-    TEST_ASSERT_EQUAL(16, ignition1EndTooth);
+    TEST_ASSERT_EQUAL(16, ignitionEndTeeth[0]);
 }
 
 void test_fordst170_newIgn_12_trig270_1()
@@ -73,13 +77,13 @@ void test_fordst170_newIgn_12_trig270_1()
     //Trigger: 36-1
     //Advance: 10
     //triggerAngle=270
-    triggerSetup_FordST170();
+    testSetup_FordST170();
     configPage4.sparkMode = IGN_MODE_WASTED;
     configPage4.triggerAngle = 270; //No trigger offset
     calculateIgnitionAngles(ignitionSchedules[0], 5, 10);
 
     triggerSetEndTeeth_FordST170();
-    TEST_ASSERT_EQUAL(7, ignition1EndTooth);
+    TEST_ASSERT_EQUAL(7, ignitionEndTeeth[0]);
 }
 
 void test_fordst170_newIgn_12_trig360_1()
@@ -88,13 +92,13 @@ void test_fordst170_newIgn_12_trig360_1()
     //Trigger: 36-1
     //Advance: 10
     //triggerAngle=360
-    triggerSetup_FordST170();
+    testSetup_FordST170();
     configPage4.sparkMode = IGN_MODE_WASTED;
     configPage4.triggerAngle = 360; //No trigger offset
     calculateIgnitionAngles(ignitionSchedules[0], 5, 10);
     
     triggerSetEndTeeth_FordST170();
-    TEST_ASSERT_EQUAL(34, ignition1EndTooth);
+    TEST_ASSERT_EQUAL(34, ignitionEndTeeth[0]);
 }
 
 void test_fordst170_newIgn_12_trigNeg90_1()
@@ -103,13 +107,13 @@ void test_fordst170_newIgn_12_trigNeg90_1()
     //Trigger: 36-1
     //Advance: 10
     //triggerAngle=-90
-    triggerSetup_FordST170();
+    testSetup_FordST170();
     configPage4.sparkMode = IGN_MODE_WASTED;
     configPage4.triggerAngle = -90; //No trigger offset
     calculateIgnitionAngles(ignitionSchedules[0], 5, 10);
 
     triggerSetEndTeeth_FordST170();
-    TEST_ASSERT_EQUAL(7, ignition1EndTooth);
+    TEST_ASSERT_EQUAL(7, ignitionEndTeeth[0]);
 }
 
 void test_fordst170_newIgn_12_trigNeg180_1()
@@ -118,13 +122,13 @@ void test_fordst170_newIgn_12_trigNeg180_1()
     //Trigger: 36-1
     //Advance: 10
     //triggerAngle=-180
-    triggerSetup_FordST170();
+    testSetup_FordST170();
     configPage4.sparkMode = IGN_MODE_WASTED;
     configPage4.triggerAngle = -180; //No trigger offset
     calculateIgnitionAngles(ignitionSchedules[0], 5, 10);
 
     triggerSetEndTeeth_FordST170();
-    TEST_ASSERT_EQUAL(16, ignition1EndTooth);
+    TEST_ASSERT_EQUAL(16, ignitionEndTeeth[0]);
 }
 
 void test_fordst170_newIgn_12_trigNeg270_1()
@@ -133,13 +137,13 @@ void test_fordst170_newIgn_12_trigNeg270_1()
     //Trigger: 36-1
     //Advance: 10
     //triggerAngle=-270
-    triggerSetup_FordST170();
+    testSetup_FordST170();
     configPage4.sparkMode = IGN_MODE_WASTED;
     configPage4.triggerAngle = -270; //No trigger offset
     calculateIgnitionAngles(ignitionSchedules[0], 5, 10);
     
     triggerSetEndTeeth_FordST170();
-    TEST_ASSERT_EQUAL(25, ignition1EndTooth);
+    TEST_ASSERT_EQUAL(25, ignitionEndTeeth[0]);
 }
 
 void test_fordst170_newIgn_12_trigNeg360_1()
@@ -148,13 +152,13 @@ void test_fordst170_newIgn_12_trigNeg360_1()
     //Trigger: 36-1
     //Advance: 10
     //triggerAngle=-360
-    triggerSetup_FordST170();
+    testSetup_FordST170();
     configPage4.sparkMode = IGN_MODE_WASTED;
     configPage4.triggerAngle = -360; //No trigger offset
     calculateIgnitionAngles(ignitionSchedules[0], 5, 10);
 
     triggerSetEndTeeth_FordST170();
-    TEST_ASSERT_EQUAL(34, ignition1EndTooth);
+    TEST_ASSERT_EQUAL(34, ignitionEndTeeth[0]);
 }
 
 void testFordST170()
