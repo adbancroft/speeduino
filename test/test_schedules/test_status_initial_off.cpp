9c0f70b49a0881c931896b1e2759f8b5244f1b42
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2023-07-02 23:11:32 -0500

    Simplify  & enable all unit tests

--- test/test_schedules/test_status_initial_off.cpp
+++ test/test_schedules/test_status_initial_off.cpp
@@ -2,135 +2,40 @@
 #include <unity.h>
 #include "scheduler.h"
 #include "../test_utils.h"
+#include "utilities.h"
 
-static void test_status_initial_off(FuelSchedule &schedule) 
+void test_status_initial_off(FuelSchedule &schedule)
 {
-    resetFuelSchedulers();
+    initialiseFuelSchedulers();
     TEST_ASSERT_EQUAL(OFF, schedule._status);
 }
 
-void test_status_initial_off_inj1(void)
+void test_status_initial_off_inj(void)
 {
-    test_status_initial_off(fuelSchedules[0]);
+    for (uint8_t i = 0; i < _countof(fuelSchedules); i++)
+    {
+        test_status_initial_off(fuelSchedules[i]);
+    }
 }
 
-void test_status_initial_off_inj2(void)
+void test_status_initial_off(IgnitionSchedule &schedule)
 {
-    test_status_initial_off(fuelSchedules[1]);
-}
-
-void test_status_initial_off_inj3(void)
-{
-    test_status_initial_off(fuelSchedules[2]);
-}
-
-void test_status_initial_off_inj4(void)
-{
-    test_status_initial_off(fuelSchedules[3]);
-}
-
-void test_status_initial_off_inj5(void)
-{
-#if ING_CHANNELS >= 5
-    test_status_initial_off(fuelSchedules[4]);
-#endif
-}
-
-void test_status_initial_off_inj6(void)
-{
-#if ING_CHANNELS >= 6
-    test_status_initial_off(fuelSchedules[5]);
-#endif
-}
-
-void test_status_initial_off_inj7(void)
-{
-#if ING_CHANNELS >= 7
-    test_status_initial_off(fuelSchedules[6]);
-#endif
-}
-
-void test_status_initial_off_inj8(void)
-{
-#if ING_CHANNELS >= 8
-    test_status_initial_off(fuelSchedules[7]);
-#endif
-}
-
-static void test_status_initial_off(IgnitionSchedule &schedule) 
-{
-    resetIgnitionSchedulers();
+    initialiseFuelSchedulers();
     TEST_ASSERT_EQUAL(OFF, schedule._status);
 }
 
-void test_status_initial_off_ign1(void)
+void test_status_initial_off_ign(void)
 {
-    test_status_initial_off(ignitionSchedules[0]);
-}
-
-void test_status_initial_off_ign2(void)
-{
-    test_status_initial_off(ignitionSchedules[1]);
-}
-
-void test_status_initial_off_ign3(void)
-{
-    test_status_initial_off(ignitionSchedules[2]);
-}
-
-void test_status_initial_off_ign4(void)
-{
-    test_status_initial_off(ignitionSchedules[3]);
-}
-
-void test_status_initial_off_ign5(void)
-{
-#if IGN_CHANNELS >= 5
-    test_status_initial_off(ignitionSchedules[4]);
-#endif
-}
-
-void test_status_initial_off_ign6(void)
-{
-#if IGN_CHANNELS >= 6
-    test_status_initial_off(ignitionSchedules[5]);
-#endif
-}
-
-void test_status_initial_off_ign7(void)
-{
-#if IGN_CHANNELS >= 7
-    test_status_initial_off(ignitionSchedules[6]);
-#endif
-}
-
-void test_status_initial_off_ign8(void)
-{
-#if IGN_CHANNELS >= 8
-    test_status_initial_off(ignitionSchedules[7]);
-#endif
+    for (uint8_t i = 0; i < _countof(ignitionSchedules); i++)
+    {
+        test_status_initial_off(ignitionSchedules[i]);
+    }
 }
 
 void test_status_initial_off(void)
 {
   SET_UNITY_FILENAME() {
-
-    RUN_TEST(test_status_initial_off_inj1);
-    RUN_TEST(test_status_initial_off_inj2);
-    RUN_TEST(test_status_initial_off_inj3);
-    RUN_TEST(test_status_initial_off_inj4);
-    RUN_TEST(test_status_initial_off_inj5);
-    RUN_TEST(test_status_initial_off_inj6);
-    RUN_TEST(test_status_initial_off_inj7);
-    RUN_TEST(test_status_initial_off_inj8);
-
-    RUN_TEST(test_status_initial_off_ign1);
-    RUN_TEST(test_status_initial_off_ign2);
-    RUN_TEST(test_status_initial_off_ign3);
-    RUN_TEST(test_status_initial_off_ign4);
-    RUN_TEST(test_status_initial_off_ign5);
-    RUN_TEST(test_status_initial_off_ign6);
-    RUN_TEST(test_status_initial_off_ign7);
-    RUN_TEST(test_status_initial_off_ign8);
+    RUN_TEST(test_status_initial_off_inj);
+    RUN_TEST(test_status_initial_off_ign);
   }
 }
