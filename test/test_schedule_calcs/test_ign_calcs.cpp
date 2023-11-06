cacaf9e86c60708a8bfaed79fec149c6efebaa61
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2023-11-06 13:37:36 -0600

    Consolidate timeout calculation

--- test/test_schedule_calcs/test_ign_calcs.cpp
+++ test/test_schedule_calcs/test_ign_calcs.cpp
@@ -33,7 +33,7 @@ struct ign_test_parameters
   int16_t expectedEndAngle;      // Expected end angle
 };
 
-void test_calc_ign_timeout(const ign_test_parameters &test_params)
+static void test_calc_ign_timeout(const ign_test_parameters &test_params)
 {
   char msg[150];
   IgnitionSchedule schedule(IGN4_COUNTER, IGN4_COMPARE);
@@ -54,7 +54,7 @@ void test_calc_ign_timeout(const ign_test_parameters &test_params)
   TEST_ASSERT_INT32_WITHIN_MESSAGE(1, test_params.running, calculateIgnitionTimeout(schedule, startAngle, test_params.channelAngle,  test_params.crankAngle), msg);
 }
 
-void test_calc_ign_timeout(const ign_test_parameters *pStart, const ign_test_parameters *pEnd)
+static void test_calc_ign_timeout(const ign_test_parameters *pStart, const ign_test_parameters *pEnd)
 {
   ign_test_parameters local;
   while (pStart!=pEnd)
@@ -65,7 +65,7 @@ void test_calc_ign_timeout(const ign_test_parameters *pStart, const ign_test_par
   }
 }
 
-void test_calc_ign_timeout_360()
+static void test_calc_ign_timeout_360()
 {
   setEngineSpeed(4000, 360);
   
@@ -270,8 +270,7 @@ void test_calc_ign_timeout_360()
 }
 
 
-
-void test_calc_ign_timeout_720()
+static void test_calc_ign_timeout_720()
 {
   setEngineSpeed(4000, 720);
 
@@ -553,7 +552,7 @@ void test_calc_ign_timeout_720()
   test_calc_ign_timeout(&test_data[0], &test_data[0]+_countof(test_data));
 }
 
-void test_rotary_channel_calcs(void)
+static void test_rotary_channel_calcs(void)
 {
     setEngineSpeed(4000, 360);
 
