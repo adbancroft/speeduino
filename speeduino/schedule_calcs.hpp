0ac3832494e2bbbf819334519d65949ebae12fa9
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2023-11-07 15:42:34 -0600

    Change channel angles to unsigned

--- speeduino/schedule_calcs.hpp
+++ speeduino/schedule_calcs.hpp
@@ -61,7 +61,7 @@ static SCHEDULE_INLINE uint32_t _calculateInjectorTimeout(const FuelSchedule &sc
 }
 
 static SCHEDULE_INLINE int16_t _calculateSparkAngle(const IgnitionSchedule &schedule, int8_t advance) {
-  int16_t angle = (schedule.channelDegrees==0 ? CRANK_ANGLE_MAX_IGN : schedule.channelDegrees) - advance;
+  int16_t angle = (schedule.channelDegrees==0U ? CRANK_ANGLE_MAX_IGN : (int16_t)schedule.channelDegrees) - advance;
   if(angle > CRANK_ANGLE_MAX_IGN) {angle -= CRANK_ANGLE_MAX_IGN;}
   return angle;
 }
