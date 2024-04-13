019425206c39d7a01750a920c171724b241a43e8
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-04-13 10:07:04 -0500

    TPS & MAP AE cannot be active at the same time: save 1 byte.

--- speeduino/corrections.cpp
+++ speeduino/corrections.cpp
@@ -40,8 +40,7 @@ static long PID_O2, PID_output, PID_AFRTarget;
 */
 static PID egoPID(&PID_O2, &PID_output, &PID_AFRTarget, configPage6.egoKP, configPage6.egoKI, configPage6.egoKD, REVERSE);
 
-static byte activateMAPDOT; //The mapDOT value seen when the MAE was activated. 
-static byte activateTPSDOT; //The tpsDOT value seen when the MAE was activated.
+static byte aeActivatedReading; //The mapDOT/tpsDOT value seen when the MAE/TAE was activated. 
 
 static bool idleAdvActive = false;
 TESTABLE_STATIC uint16_t AFRnextCycle;
@@ -341,7 +340,7 @@ static inline void mapOnTimeoutExpired(void) {
 }
 
 static inline bool mapShouldResetAe(void) {
-  return (uint16_t)abs(currentStatus.mapDOT) > activateMAPDOT; 
+  return (uint16_t)abs(currentStatus.mapDOT) > aeActivatedReading; 
 }
 
 static inline bool mapShouldStartAe(void) { 
@@ -357,7 +356,7 @@ static inline uint16_t mapComputeAe(void) {
     aeEnrichment = calcAccelEnrichment(table2D_getValue(&maeTable, toRawU8(MAP_DOT, currentStatus.mapDOT)));
   } 
   
-  activateMAPDOT = abs(currentStatus.mapDOT);
+  aeActivatedReading = abs(currentStatus.mapDOT);
   
   return aeEnrichment;
 }
@@ -406,7 +405,7 @@ static inline void tpsOnTimeoutExpired(void) {
 }
 
 static inline bool tpsShouldResetAe(void) { 
-  return (uint16_t)abs(currentStatus.tpsDOT) > activateTPSDOT; 
+  return (uint16_t)abs(currentStatus.tpsDOT) > aeActivatedReading; 
 }
 
 static inline bool tpsShouldStartAe(void) { 
@@ -422,7 +421,7 @@ static inline uint16_t tpsComputeAe(void) {
   } else {
     aeEnrichment = calcAccelEnrichment(table2D_getValue(&taeTable, toRawU8(TPS_DOT, currentStatus.tpsDOT))); 
   }
-  activateTPSDOT = abs(currentStatus.tpsDOT);
+  aeActivatedReading = abs(currentStatus.tpsDOT);
 
   return aeEnrichment;
 }
