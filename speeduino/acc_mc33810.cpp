5fa51b2f9b497b0ef183f631988795cdda0e0d9c
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-07-11 10:27:25 -0500

    MC33810 - use array for channel bit indices

--- speeduino/acc_mc33810.cpp
+++ speeduino/acc_mc33810.cpp
@@ -2,24 +2,8 @@
 
 #if defined(OUTPUT_CONTROL_SUPPORTED)
 
-uint8_t MC33810_BIT_INJ1 = 1;
-uint8_t MC33810_BIT_INJ2 = 2;
-uint8_t MC33810_BIT_INJ3 = 3;
-uint8_t MC33810_BIT_INJ4 = 4;
-uint8_t MC33810_BIT_INJ5 = 5;
-uint8_t MC33810_BIT_INJ6 = 6;
-uint8_t MC33810_BIT_INJ7 = 7;
-uint8_t MC33810_BIT_INJ8 = 8;
-
-uint8_t MC33810_BIT_IGN1 = 1;
-uint8_t MC33810_BIT_IGN2 = 2;
-uint8_t MC33810_BIT_IGN3 = 3;
-uint8_t MC33810_BIT_IGN4 = 4;
-uint8_t MC33810_BIT_IGN5 = 5;
-uint8_t MC33810_BIT_IGN6 = 6;
-uint8_t MC33810_BIT_IGN7 = 7;
-uint8_t MC33810_BIT_IGN8 = 8;
-
+uint8_t MC33810_BIT_INJ[INJ_CHANNELS] = { 0, 1, 2, 3, 4, 5, 6, 7 };
+uint8_t MC33810_BIT_IGN[IGN_CHANNELS] = { 0, 1, 2, 3, 4, 5, 6, 7 };
 byte pinMC33810_1_CS;
 byte pinMC33810_2_CS;
 ioPort portMC33810_1_CS;
