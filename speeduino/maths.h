354e7e1b778596e7e15d225150c0898cdd7364e8
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2024-08-07 10:44:46 -0500

    Add & unit test udiv_16_8

--- speeduino/maths.h
+++ speeduino/maths.h
@@ -221,13 +221,20 @@ static inline int16_t nudge(int16_t min, int16_t max, int16_t value, int16_t nud
 //This is a new version that allows for out_min
 #define fastMap10Bit(x, out_min, out_max) ( rshift<10>( (uint32_t)(x) * ((out_max)-(out_min)) ) + (out_min))
 
-#if defined(CORE_AVR) || defined(ARDUINO_ARCH_AVR)
-
-static inline bool udiv_is16bit_result(uint32_t dividend, uint16_t divisor) {
-  return divisor>(uint16_t)(dividend>>16U);
+/**
+ * @brief Will the result of division fit into TDivisor?
+ * 
+ * @tparam TDividend Dividend type 
+ * @tparam TDivisor Divisor type
+ * @param dividend The dividend (numerator)
+ * @param divisor The divisor (denominator)
+ * @return true If the result of dividend/divisor will fit into TDivisor, false otherwise
+ */
+template <typename TDividend, typename TDivisor>
+static constexpr inline bool udivDivResultFitsInDivisor(TDividend dividend, TDivisor divisor) {
+  return divisor>(TDivisor)( dividend>> ((sizeof(TDividend)-sizeof(TDivisor))*8U));
 }
 
-#endif
 /**
  * @brief Optimised division: uint32_t/uint16_t => uint16_t
  * 
@@ -247,7 +254,7 @@ static inline uint16_t udiv_32_16 (uint32_t dividend, uint16_t divisor)
 {
 #if defined(CORE_AVR) || defined(ARDUINO_ARCH_AVR)
 
-    if (divisor==0U || !udiv_is16bit_result(dividend, divisor)) { return UINT16_MAX; }
+    if (divisor==0U || !udivDivResultFitsInDivisor(dividend, divisor)) { return UINT16_MAX; }
 
     #define INDEX_REG "r16"
 
@@ -297,6 +304,58 @@ static inline uint16_t udiv_32_16_closest(uint32_t dividend, uint16_t divisor)
     return udiv_32_16(dividend, divisor);
 #else
     return (uint16_t)UDIV_ROUND_CLOSEST(dividend, (uint32_t)divisor, uint32_t);
+#endif
+}
+
+
+/**
+ * @brief Optimised division: uint16_t/uint8_t => uint8_t
+ * 
+ * Optimised division of unsigned 16-bit by unsigned 8-bit when it is known
+ * that the result fits into unsigned 8-bit.
+ * 
+ * ~60% quicker than raw 16/16 => 16 division on ATMega
+ * 
+ * @note Bad things will likely happen if the result doesn't fit into 8-bits.
+ * @note Copied from https://stackoverflow.com/a/66593564
+ * 
+ * @param dividend The dividend (numerator)
+ * @param divisor The divisor (denominator)
+ * @return uint8_t 
+ */
+static inline uint8_t udiv_16_8 (uint16_t dividend, uint8_t divisor)
+{
+#if defined(CORE_AVR) || defined(ARDUINO_ARCH_AVR)
+
+    if (divisor==0U || !udivDivResultFitsInDivisor(dividend, divisor)) { return UINT8_MAX; }
+
+    #define INDEX_REG "r16"
+
+    asm(
+        "    ldi " INDEX_REG ", 8 ; bits = 8\n\t"
+        "0:\n\t"
+        "    lsl  %A0      ; shift rem:quot\n\t"
+        "    rol  %B0      ;  left by 1\n\t"
+        "    brcs 1f       ; if carry out, rem > divisor\n\t"
+        "    cpc  %B0, %A1 ; is rem less than divisor?\n\t"
+        "    brcs 2f       ; yes, when carry out\n\t"
+        "1:\n\t"
+        "    sub  %B0, %A1 ; compute rem -= divisor\n\t"
+        "    ori  %A0, 1   ; record quotient bit as 1\n\t"
+        "2:\n\t"
+        "    dec  " INDEX_REG "     ; bits--\n\t"
+        "    brne 0b        ; until bits == 0"
+        : "=d" (dividend) 
+        : "d" (divisor) , "0" (dividend) 
+        : INDEX_REG
+    );
+
+    // Lower word contains the quotient, upper word contains the remainder.
+    return dividend & 0xFFFFU;
+#else
+    // The non-AVR platforms are all fast enough (or have built in hardware dividers)
+    // so just fall back to regular 16-bit division.
+    return dividend / divisor;
 #endif
 }
 
