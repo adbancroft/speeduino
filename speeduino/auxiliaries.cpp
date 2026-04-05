6b242d4d530c1bfd86df9686a64a62a2391bbb50
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-04-05 17:17:21 -0500

    Convert statuses::vvt?Dityto uint8_t - it doesn't have to be `long` for the PID calcs anymore

--- speeduino/auxiliaries.cpp
+++ speeduino/auxiliaries.cpp
@@ -1026,10 +1026,13 @@ void vvtControl(void)
         else
         {
           //If not already at target angle, calculate new value from PID
-          bool PID_compute = vvtPID.Compute(millis(), currentStatus.vvt1Angle, &currentStatus.vvt1Duty);
-          //vvt_pwm_target_value = percentage(40, vvt_pwm_max_count);
-          //if (currentStatus.vvt1Angle > currentStatus.vvt1TargetAngle) { vvt_pwm_target_value = 0; }
-          if(PID_compute == true) { vvt1_pwm_value = halfPercentage(currentStatus.vvt1Duty, vvt_pwm_max_count); }
+          long pidOutput = 0;
+          bool PID_compute = vvtPID.Compute(millis(), currentStatus.vvt1Angle, &pidOutput);
+          if(PID_compute == true) 
+          { 
+            currentStatus.vvt1Duty = (uint8_t)pidOutput;
+            vvt1_pwm_value = halfPercentage(currentStatus.vvt1Duty, vvt_pwm_max_count); 
+          }
           currentStatus.vvt1AngleError = false;
         }
 
@@ -1061,8 +1064,13 @@ void vvtControl(void)
           else
           {
             //If not already at target angle, calculate new value from PID
-            bool PID_compute = vvt2PID.Compute(millis(), currentStatus.vvt2Angle, &currentStatus.vvt2Duty);
-            if(PID_compute == true) { vvt2_pwm_value = halfPercentage(currentStatus.vvt2Duty, vvt_pwm_max_count); }
+            long pidOutput = 0;
+            bool PID_compute = vvt2PID.Compute(millis(), currentStatus.vvt2Angle, &pidOutput);
+            if(PID_compute == true) 
+            { 
+              currentStatus.vvt2Duty = (uint8_t)pidOutput;
+              vvt2_pwm_value = halfPercentage(currentStatus.vvt2Duty, vvt_pwm_max_count); 
+            }
             currentStatus.vvt2AngleError = false;
           }
         }
