e054d1d8d8ce9e04cfd3f987610856eaeb6e40a4
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-08-06 16:04:05 -0500

    Handle the case of flex boost correction making the boost target  overflow.

--- test/test_boostcontrol/test_boostcontrol.cpp
+++ test/test_boostcontrol/test_boostcontrol.cpp
@@ -25,6 +25,9 @@ static void test_boost_disabled(void)
     boostControlCore();
 
     TEST_ASSERT_EQUAL(0, currentStatus.flexBoostCorrection);
+    TEST_ASSERT_EQUAL(0, currentStatus.boostDuty);
+    TEST_ASSERT_EQUAL(0, currentStatus.boostTarget);
+    TEST_ASSERT_EQUAL(0, boost_pwm_target_value);
 }
 
 static void setup_boost_tune(bool fullPid, uint8_t vssMode, uint8_t boostType, uint8_t gearMode)
@@ -84,6 +87,8 @@ static void test_boost_ol_duty_clamp(void)
     currentStatus.gear = gear;
     boostControlCore();
     TEST_ASSERT_EQUAL(10000, currentStatus.boostDuty);
+    TEST_ASSERT_EQUAL(0, currentStatus.boostTarget);
+    TEST_ASSERT_EQUAL(41248, boost_pwm_target_value);
   }
 
     // Invalid gear
@@ -93,6 +98,8 @@ static void test_boost_ol_duty_clamp(void)
     currentStatus.gear = 0;
     boostControlCore();
     TEST_ASSERT_EQUAL(0, currentStatus.boostDuty);
+    TEST_ASSERT_EQUAL(0, currentStatus.boostTarget);
+    TEST_ASSERT_EQUAL(0, boost_pwm_target_value);
   }
 }
 
@@ -107,6 +114,8 @@ static void test_ol_zero_duty(void)
 
   TEST_ASSERT_EQUAL(0, currentStatus.boostDuty);
   TEST_ASSERT_TRUE(boost_pin._pin.isPinLow());
+  TEST_ASSERT_EQUAL(0, currentStatus.boostTarget);
+  TEST_ASSERT_EQUAL(0, boost_pwm_target_value);
 }
 
 static void run_ol_tests(void)
@@ -154,7 +163,8 @@ static void test_boost_cl_target_clamp(void)
     TEST_ASSERT_EQUAL(511, currentStatus.boostTarget);
     TEST_ASSERT_EQUAL(642, currentStatus.boostDuty);
     TEST_ASSERT_EQUAL(0, currentStatus.flexBoostCorrection);
-  }
+    TEST_ASSERT_EQUAL(2648, boost_pwm_target_value);
+}
 
   // Invalid gear
   currentStatus.boostTarget = 1;
@@ -164,6 +174,8 @@ static void test_boost_cl_target_clamp(void)
   boostControlCore();
   TEST_ASSERT_EQUAL(0, currentStatus.boostTarget);
   TEST_ASSERT_EQUAL(0, currentStatus.boostDuty);
+  TEST_ASSERT_EQUAL(0, currentStatus.flexBoostCorrection);
+  TEST_ASSERT_EQUAL(0, boost_pwm_target_value);
 }
 
 static void test_cl_flexcorrection(void)
@@ -183,6 +195,27 @@ static void test_cl_flexcorrection(void)
   TEST_ASSERT_EQUAL_INT16(77, currentStatus.flexBoostCorrection);
   TEST_ASSERT_EQUAL_INT16((boostTable.values[0]*2)+77, currentStatus.boostTarget);
   TEST_ASSERT_EQUAL(577, currentStatus.boostDuty);
+  TEST_ASSERT_EQUAL(2380, boost_pwm_target_value);
+}
+
+static void test_cl_flexcorrection_negative(void)
+{
+  setup_boost_tune(false, VSS_MODE_EXTERNAL_MI, CLOSED_LOOP_BOOST, BOOST_BY_GEAR_OFF);
+  configPage2.flexEnabled = true;
+  populate_2dtable(&flexBoostTable, (int16_t)-77, (uint8_t)50);
+  fill_table_values(boostTable, 5);
+
+  initialiseBoost(TEST_BOOST_PIN);
+
+  currentStatus.LOOP_TIMER = 0xFF;
+  currentStatus.flexBoostCorrection = 99;
+  boostPID.initialize(currentStatus.MAP);
+  boostControl();
+
+  TEST_ASSERT_EQUAL_INT16(-77, currentStatus.flexBoostCorrection);
+  TEST_ASSERT_EQUAL_INT16(0, currentStatus.boostTarget);
+  TEST_ASSERT_EQUAL(0, currentStatus.boostDuty);
+  TEST_ASSERT_EQUAL(0, boost_pwm_target_value);
 }
 
 static void test_cl_boost_constant_gear(uint8_t gearNum, uint8_t &boostGear)
@@ -190,11 +223,10 @@ static void test_cl_boost_constant_gear(uint8_t gearNum, uint8_t &boostGear)
   currentStatus.LOOP_TIMER = 0xFF;
   currentStatus.boostTarget = 1;
   currentStatus.gear = gearNum;
-  boostGear = 3;
+  boostGear = gearNum*7;
   boostPID.initialize(currentStatus.MAP);
   boostControlCore();
-  TEST_ASSERT_EQUAL(boostGear << 1U, currentStatus.boostTarget);
-  TEST_ASSERT_EQUAL(541, currentStatus.boostDuty);
+  TEST_ASSERT_UINT16_WITHIN(1, boostGear << 1U, currentStatus.boostTarget);
 }
 
 static void test_cl_boost_constant_gear(void)
@@ -267,6 +299,7 @@ static void run_cl_tests(void)
 {
   RUN_TEST_P(test_boost_cl_target_clamp);
   RUN_TEST_P(test_cl_flexcorrection);
+  RUN_TEST_P(test_cl_flexcorrection_negative);
   RUN_TEST_P(test_cl_boost_constant_gear);
   RUN_TEST_P(test_cl_boost_control_baro);
   RUN_TEST_P(test_cl_boost_control_fixed);
