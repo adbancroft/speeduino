94864402a47d7cdbc8f764a34d5f49f0331e9a25
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2026-08-26 13:00:44 -0500

    Fix minor Sonar issues

--- speeduino/src/controllers/boost/boostController.cpp
+++ speeduino/src/controllers/boost/boostController.cpp
@@ -70,7 +70,7 @@ static uint16_t getBoostDuty(const statuses &current, const config2 &page2, cons
   uint16_t duty = 0;
   if (getBoostByGearMode(page2, page9) == BoostByGearMode::Percent)
   {
-    duty = ( ((uint16_t)getBoostByGearFactor(current, page9) * (uint16_t)get3DTableValue(&boostTable, (current.TPS * 2U), current.RPM))  ) << 2;
+    duty = ((uint16_t)getBoostByGearFactor(current, page9) * (uint16_t)get3DTableValue(&boostTable, (current.TPS * 2U), current.RPM)) << 2;
   }
   else if (getBoostByGearMode(page2, page9) == BoostByGearMode::Constant)
   {
@@ -190,7 +190,11 @@ TESTABLE_STATIC void boostControlCore(statuses &current, const config2 &page2, c
         //Boost control needs to have a high duty cycle if control is below threshold (baro or fixed value). This ensures the waste gate is closed as much as possible, this build boost as fast as possible.
         current.boostDuty = page15.boostDCWhenDisabled*100;
       } //MAP above boost + hyster
-    } //Open / Cloosed loop   
+    } //Open / Cloosed loop
+    else
+    {
+      // Unknown boost type
+    } 
   }
   else { // Disable timer channel and zero the flex boost correction status
     boostPID.initialize(current.MAP); //This resets the ITerm value to prevent rubber banding
